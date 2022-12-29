0e0cca2c9d44810ce43fb9088d9010eb0152a18e
deepak-hp <deepakhp19@gmail.com>
2022-12-29 20:05:02 +0530

    Create nested React Elements

--- app/reactScript.js
+++ app/reactScript.js
@@ -1,5 +1,7 @@
-const reactHeadring1 = React.createElement("h1", { id: "reactElement" }, "Hello World from React");
-const reactHeadring2 = React.createElement("h2", { id: "reactElement" }, "Hello World from React again");
+const reactHeadring1 = React.createElement("h1", { id: "reactElement", key: "1" }, "Hello World from React");
+const reactHeadring2 = React.createElement("h2", { id: "reactElement", key: "2" }, "Hello World from React again");
+const contianer = React.createElement("div", {}, [reactHeadring1, reactHeadring2])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render([reactHeadring1, reactHeadring2])
+root.render(contianer)
 
