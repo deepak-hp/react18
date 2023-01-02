d11ad5c809da3837e374ec82541a09cb84ed1c53
deepak-hp <deepakhp19@gmail.com>
2023-01-03 01:28:32 +0530

    Chapter 02 project assignment

---
+++ React18_Chapter-02/app/reactScript.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const reactHeadring1 = React.createElement(
+    'h1',
+    { id: 'reactElement', key: '1' },
+    'Hello World from React'
+);
+const reactHeadring2 = React.createElement(
+    'h2',
+    { id: 'reactElement', key: '2' },
+    'Hello World from React again'
+);
+const contianer = React.createElement('div', {}, [
+    reactHeadring1,
+    reactHeadring2,
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(contianer);
---
+++ React18_Chapter-02/docs/theory.md
@@ -0,0 +1,3 @@
+# What is [`NPM`](https://www.freecodecamp.org/news/what-is-npm-a-node-package-manager-tutorial-for-beginners/)?
+
+- [NPM](https://www.npmjs.com/) is a software registry(library) and also a package manager where we can find amazing packages written and shared by other developers to help the javascript community and it is open source and free to use.
