ed120f233538b3b4531079157b5447e7464a135f
deepak-hp <deepakhp19@gmail.com>
2023-02-14 18:59:06 +0530

    Added api call and shimmer support

--- React18_Chapter-06/app/src/components/Body/Body.jsx
+++ React18_Chapter-06/app/src/components/Body/Body.jsx
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { restaurantList } from "../../constants"
 import Card from "../Card";
 import Shimmer from "../Shimmer";
 
 const API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.9299299&lng=75.568101&page_type=DESKTOP_WEB_LISTING";
 
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(restaurantList);
+    const [restaurants, setRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
     const getData = async () => {
-        const data = await fetch(API_URL)
-        console.log(data);
-        const restaurantList = JSON.parse(data?.cards);
-        console.log("restaurantList: ", restaurantList);
+        const res = await fetch(API_URL)
+        const jsonRes = await res.json();
+        if (jsonRes?.data?.cards[2]?.data?.data?.cards) {
+            setRestaurants(jsonRes.data.cards[2].data.data.cards);
+            setFilteredRestaurants(jsonRes.data.cards[2].data.data.cards);
+        }
     }
 
 
@@ -28,28 +30,29 @@ const Body = () => {
 
     const handleSearch = (term) => {
         if (term) {
-            const filteredList = restaurantList.filter((res) => {
-                if (res?.data?.name?.toLowerCase()?.includes(term.trim().toLowerCase())) return res;
+            const filteredList = restaurants.filter((res) => {
+                if (res?.data?.name?.trim()?.toLowerCase()?.includes(term.trim().toLowerCase())) return res;
             })
             if (filteredList.length)
-                setRestaurants(filteredList);
+                setFilteredRestaurants(filteredList);
             else
-                setRestaurants([]);
+                setFilteredRestaurants([]);
 
         } else
-            setRestaurants(restaurantList);
+            setFilteredRestaurants(restaurants);
 
     }
 
-    if (restaurantList.length === 0) {
+    if (restaurants?.length === 0) {
         return <Shimmer />
     }
+    console.log(filteredRestaurants);
 
     return (
         <div className='section'>
             <SearchBar handleSearch={handleSearch} />
             <div className="listContainer">
-                {restaurants.length ? restaurants.map((restaurant) => (
+                {filteredRestaurants.length > 0 ? filteredRestaurants.map((restaurant) => (
                     <Card key={restaurant?.data?.id} restaurant={restaurant?.data} />
                 ))
                     : <h1>No Restaurant found!!</h1>
--- React18_Chapter-06/app/src/components/Header/Header.jsx
+++ React18_Chapter-06/app/src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
 import logo from '../../../assets/cater_factory.jpg';
 import { Logo, NavItems } from './NavBar';
+
 const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div className='header'>
             <Logo logo={logo} />
             <NavItems />
+            {isLoggedIn ?
+                <button onClick={() => setIsLoggedIn(false)}>Log out</button>
+                :
+                <button onClick={() => setIsLoggedIn(true)}>Log In</button>
+            }
         </div>
     );
 };
